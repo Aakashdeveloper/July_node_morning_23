96eceb0fa99eccb2ce6d964252eaa62d5f7c7c00
aakashdeveloper <ahanda205@gmail.com>
2023-07-17 10:08:58 +0530

    restapi

--- zomapi/app.js
+++ zomapi/app.js
@@ -6,7 +6,7 @@ dotenv.config();
 let bodyParser = require('body-parser');
 let cors = require('cors');
 let port = process.env.PORT;
-let {dbConnect,getData} = require('./controller/dbController')
+let {dbConnect,getData,getDataSort,getDataSortLimit} = require('./controller/dbController')
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -57,6 +57,50 @@ app.get('/mealType',async(req,res) => {
     let query = {};
     let collection = 'mealType';
     let output = await getData(collection,query);
+    res.send(output)
+})
+
+
+//filters
+app.get('/filters/:mealId',async(req,res) => {
+    let query = {};
+    let mealId = Number(req.params.mealId);
+    let cuisineId = Number(req.query.cuisineId);
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    let sort = {cost:1};
+    let skip = 0;
+    let limit = 1000000000;
+    if(req.query.skip && req.query.limit){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit)
+    }
+    if(req.query.sort){
+        sort={cost:req.query.sort}
+    }
+    if(cuisineId){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            "cuisines.cuisine_id":cuisineId
+        }
+    }else if(lcost && hcost){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+    let collection = "restaurants";
+    let output = await getDataSortLimit(collection,query,sort,skip,limit);
+    res.send(output);
+})
+
+//restaurants details
+app.get('/details/:id', async(req,res) => {
+    //let id = Number(req.params.id);
+    //let query = {restaurant_id:id};
+    let _id = mongo.ObjectId(req.params.id);
+    let query = {_id}
+    let output = await getData("restaurants",query)
     res.send(output)
 })
 
