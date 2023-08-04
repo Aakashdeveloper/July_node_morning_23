1a4eca19cadcbeb4d685c82eac61ca98d4c37e59
aakashdeveloper <ahanda205@gmail.com>
2023-08-04 09:55:11 +0530

    postgress

---
+++ graphql/Schema/Schema.js
@@ -0,0 +1,42 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLString
+} = graphql
+
+const ProductType = new GraphQLObjectType({
+    name:'Products',
+    fields:({
+        id: {type:GraphQLInt},
+        product_name: {type:GraphQLString},
+        category:  {type:GraphQLString},
+        category_id: {type:GraphQLInt},
+        Price: {type:GraphQLInt},
+        Size:  {type:GraphQLString},
+        Image: {type:GraphQLString},
+        Color:  {type:GraphQLString},
+        company:  {type:GraphQLString}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:({
+        Products:{
+            type:ProductType,
+            args:{id:{type:GraphQLInt}},
+            resolve(parentValue,args){
+                return axios.get(`http://localhost:8211/products/${args.id}`)
+                .then((res) =>  res.data)
+            }
+        }
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
--- graphql/app.js
+++ graphql/app.js
@@ -3,3 +3,14 @@ const port = 8600;
 const app = express();
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./Schema/Schema');
+
+app.use('/graphql',
+    graphqlHTTP({
+        schema:schema,
+        graphiql:true
+    })
+)
+
+app.listen(port,() => {
+    console.log(`Listing to port ${port}`)
+})
